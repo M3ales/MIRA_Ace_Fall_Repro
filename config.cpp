f2b6bf34c4c44d919fe7940fed06160d9536a777
m3ales <mattfdavies12@gmail.com>
2023-06-02 18:16:22 +0200

    feat: add minimum reproduction

---
+++ config.cpp
@@ -0,0 +1,27 @@
+class CfgPatches {
+	class MIRA_Ace_Fall_Handler_Min_Repro
+	{
+		name = "Ace Fall Handler Minimum Reproduction";
+		author = "M3ales";
+        url = "https://github.com/M3ales/MIRA_Ace_Fall_Repro";
+		requiredAddons[] = {"ace_medical"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class ACE_Medical_Injuries {
+    class damageTypes {
+        class woundHandlers;
+        class falling {
+            class woundHandlers: woundHandlers {
+                MIRA_Ace_Fall_Handler_Min_Repro = "{ diag_log ""You fell out of luck"" }";
+            };
+        };
+        class collision {
+            class woundHandlers: woundHandlers {
+                MIRA_Ace_Fall_Handler_Min_Repro = "{ diag_log ""You took collision damage"" }";
+            };
+        };
+    };
+};
